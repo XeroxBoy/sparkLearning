a137ba7affc2a1ecfeb82bcfcbd90be8db825015
XeroxBoy <827312773@qq.com>
2019-04-18 11:04:30 +0800

    still reading books

--- src/main/scala/com/sx/sparkTest00.scala
+++ src/main/scala/com/sx/sparkTest00.scala
@@ -1,10 +1,19 @@
 package com.sx
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 class sparkTest00 {
 
 }
 object sparkTest00{
   def main(args: Array[String]): Unit = {
+        val sparkConf = new SparkConf().setAppName("pair rdd").setMaster("local")
+        val sc = new SparkContext(sparkConf)
+        val lines = sc.parallelize(List("scala ide","java ide","python ide","scala ide"))
+        val pairs = lines.map(x =>(x.split(" ")(0),1)).reduceByKey((x,y)=>x+y)
+        for (pair <- pairs){
+          println(pair)
+        }
 
   }
 }
