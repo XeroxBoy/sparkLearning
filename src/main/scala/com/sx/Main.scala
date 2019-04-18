e2b55e8a25dbc1edbe8db75c07750f8818905c72
XeroxBoy <sb827312773@outlook.com>
2019-04-18 22:07:37 +0800

    still reading books

--- src/main/scala/com/sx/Main.scala
+++ src/main/scala/com/sx/Main.scala
@@ -20,8 +20,18 @@ object Main{
 //    dfs.groupBy("age").count().show()
     dfs.write.parquet("employee.parquet")
     val parqfile = sqlsc.read.parquet("employee.parquet")
+    val parqfile2 = sqlsc.load("employee.parquet")
     parqfile.registerTempTable("employee")
     val allRecords = sqlsc.sql("SELECT * FROM employee")
     allRecords.show()
+    //数据保存
+    //方式一
+    parqfile.write.json("E:\\111")
+    parqfile.write.parquet("E:\\222")
+    //方式二
+    parqfile.write.format("json").save("E:\\333")
+    parqfile.write.format("parquet").save("E:\\444")
+    //方式三
+    parqfile.write.save("E:\\555")
   }
 }
